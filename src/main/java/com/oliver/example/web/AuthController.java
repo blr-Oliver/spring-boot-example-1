fe98a5c47db5c9283de164f9248e273f618b9714
blr-Oliver <vasily.liaskovsky@gmail.com>
2023-03-31 02:03:05 +0300

    support both form and json for registration

--- src/main/java/com/oliver/example/web/AuthController.java
+++ src/main/java/com/oliver/example/web/AuthController.java
@@ -4,6 +4,11 @@ import com.oliver.example.entity.Account;
 import com.oliver.example.repository.AccountRepository;
 import com.oliver.example.service.AccountService;
 import com.oliver.example.web.dto.LoginDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -35,8 +40,31 @@ public class AuthController {
     // this will never be called
   }
 
-  @RequestMapping(path = "/register", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE, MediaType.APPLICATION_JSON_VALUE})
-  public Account register(LoginDto registration, HttpServletResponse response) {
+  @Operation(
+      method = "POST",
+      requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+          required = true,
+          content = {
+              @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = LoginDto.class)),
+              @Content(mediaType = MediaType.APPLICATION_FORM_URLENCODED_VALUE, schema = @Schema(implementation = LoginDto.class))
+          }
+      ),
+      responses = {
+          @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Account.class)))
+      }
+  )
+  @RequestMapping(path = "/register", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Account registerWithForm(@Parameter(hidden = true) LoginDto registration, HttpServletResponse response) {
+    return doRegister(registration, response);
+  }
+
+  @Operation(hidden = true)
+  @RequestMapping(path = "/register", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Account registerWithJson(@RequestBody LoginDto registration, HttpServletResponse response) {
+    return doRegister(registration, response);
+  }
+
+  private Account doRegister(LoginDto registration, HttpServletResponse response) {
     Optional<Account> existing = accountRepository.findByEmail(registration.getUsername());
     if (existing.isPresent()) {
       response.setStatus(HttpStatus.CONFLICT.value());
