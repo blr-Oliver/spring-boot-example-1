d68e9430477c4be19b2f35dd73c63160dbb4a9a9
blr-Oliver <vasily.liaskovsky@gmail.com>
2023-03-30 19:55:51 +0300

    /register endpoint + hidden password

--- src/main/java/com/oliver/example/web/AuthController.java
+++ src/main/java/com/oliver/example/web/AuthController.java
@@ -1,6 +1,9 @@
 package com.oliver.example.web;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,12 +15,16 @@ public class AuthController {
 
   @RequestMapping(path = "/login", method = RequestMethod.POST)
   public void login(@RequestParam(name = "username") String username,
-                    @RequestParam(name = "password") String password) {
+                    @RequestParam(name = "password") @Parameter(schema = @Schema(format = "password")) String password) {
     // this will never be called
   }
 
   @RequestMapping(path = "/logout", method = RequestMethod.POST)
   public void logout() {
     // this will never be called
+  }
+
+  @RequestMapping(path = "/register", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE, MediaType.APPLICATION_JSON_VALUE})
+  public void register() {
   }
 }
